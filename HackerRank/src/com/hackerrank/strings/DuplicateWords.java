cc251658f6d812674f2b1bd6f235fd680608e767
Rafael Scortegagna <rscortegagna@gmail.com>
2018-10-29 21:29:07 -0300

    Duplicate Words challenge

---
+++ HackerRank/src/com/hackerrank/strings/DuplicateWords.java
@@ -0,0 +1,45 @@
+package com.hackerrank.strings;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * https://www.hackerrank.com/challenges/duplicate-word/problem
+ */
+public class DuplicateWords {
+
+    public static void main(String[] args) {
+
+        String pattern = "(?i)\\b([a-z]+)\\b(?:\\s+\\1\\b)+";
+
+        Pattern p = Pattern.compile(pattern, Pattern.MULTILINE + Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+
+            Matcher m = p.matcher(input);
+
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find( )) {
+                input = input.replaceAll("(?i)\\b([a-z]+)\\b(?:\\s+\\1\\b)+", "$1");
+            }
+
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+
+        in.close();
+    }
+}
+
+/*
+Goodbye bye bye world world world
+Sam went went to to to his business
+Reya is is the the best player in eye eye game
+in inthe
+Hello hello Ab aB
+ */
